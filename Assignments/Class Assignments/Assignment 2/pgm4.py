cb15f9238c53f9f31d8426c5518e828818192662
Ashwin Raikar <ashwin.raikar77@gmail.com>
2019-05-24 14:57:19 +0530

    Update pgm4.py

--- Assignments/Class Assignments/Assignment 2/pgm4.py
+++ Assignments/Class Assignments/Assignment 2/pgm4.py
@@ -1,20 +1,27 @@
 import re
 
-fhand = open('mbox-short.txt')
+fhand = open('D:/Downloads/Documents/mbox.txt')
 
 count = dict()
 
 for line in fhand:
   line = line.rstrip()
-  if re.search('^From .* [0-9][0-9]:'.line):
-      x = re.findall('^From .* ([0-9][0-9]):', line)
+  if re.search('^From .* [0-9][0-9]:', line):
+      x = re.findall('^From [a-zA-Z]\S+@\S+[a-zA-Z] ([a-zA-Z][a-zA-Z][a-zA-Z])', line)
       
   if len(x) > 0:
-    print(x)
-    
+#    print(x)   
     for no in x:
-      if no not in count:
-        count[line] = 1
-       else:
-        count[line] += 1
+        if no not in count:
+            count[no] = 1
+        else:
+            count[no] += 1
+            
+lst = list()
+for key, val in list(count.items()):
+  lst.append((val, key))
 
+lst.sort()
+
+for key, val in lst:
+  print('Day',val,'Count: ',key)
