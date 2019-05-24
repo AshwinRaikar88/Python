e1bd66461148f1c3e6e31c2fe30a0bbc801b576c
Ashwin Raikar <ashwin.raikar77@gmail.com>
2019-05-24 12:51:30 +0530

    Create pgm3.py

---
+++ Assignments/Class Assignments/Assignment 2/pgm3.py
@@ -0,0 +1,15 @@
+#Assignment Program 3
+
+number=list()
+
+while(True):
+    num=input("Enter a number:")
+
+    if(num=="done"):
+        print("Maximum value:",max(number),"Minimum value:",min(number))
+        break
+    try:
+            number.append(int(num))
+            
+    except:
+        print("\nInvalid input")
