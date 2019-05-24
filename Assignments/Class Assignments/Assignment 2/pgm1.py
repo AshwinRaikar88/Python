558c6b3230d1609f280040645db2c55599577624
Ashwin Raikar <ashwin.raikar77@gmail.com>
2019-05-24 12:20:16 +0530

    Create pgm1.py

---
+++ Assignments/Class Assignments/Assignment 2/pgm1.py
@@ -0,0 +1,25 @@
+import string
+
+fhand = open('abc.txt')
+counts = dict()
+
+for line in fhand:
+  line = line.translate(str.maketrans('', '', string.punctuation))
+  line = line.lower()
+  words = line.split()
+    for word in words:
+      if word not in counts:
+        counts[word] = 1
+      else:
+        counts[word] += 1
+
+# Sort the dictionary by value
+lst = list()
+
+for key, val in list(counts.items()):
+  lst.append((val, key))
+
+lst.sort()
+
+for key, val in lst:
+  print(key, val)
