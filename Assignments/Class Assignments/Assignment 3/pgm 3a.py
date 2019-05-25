ba29ebe430badd0586b44051ae13707c20f544bd
Ashwin Raikar <ashwin.raikar77@gmail.com>
2019-05-25 18:50:25 +0530

    Update pgm 3a.py
    3. Execute the program which retrieves a particular file on server using URLlib and
    Sockets library.

--- Assignments/Class Assignments/Assignment 3/pgm 3a.py
+++ Assignments/Class Assignments/Assignment 3/pgm 3a.py
@@ -1,3 +1,8 @@
+'''3. Execute the program which retrieves a particular file on server using URLlib and
+Sockets library.'''
+
+# Using URLlib library: 
+
 import urllib.request
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
