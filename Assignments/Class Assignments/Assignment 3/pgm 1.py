56fb644253b468630575f111681d692e212321e9
Ashwin Raikar <ashwin.raikar77@gmail.com>
2019-05-25 19:13:07 +0530

    Update pgm 1.py
    1. Write the socket program so that it counts the number of characters it has
    received and stops displaying any text after it has shown 3000 characters. The
    program should retrieve the entire document and count the total number of
    characters and display the count of the number of characters at the end of the
    document.

--- Assignments/Class Assignments/Assignment 3/pgm 1.py
+++ Assignments/Class Assignments/Assignment 3/pgm 1.py
@@ -1,3 +1,9 @@
+'''1. Write the socket program so that it counts the number of characters it has
+received and stops displaying any text after it has shown 3000 characters. The
+program should retrieve the entire document and count the total number of
+characters and display the count of the number of characters at the end of the
+document.'''
+
 import socket
 import time
 
